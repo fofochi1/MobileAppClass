968377ebc0738da069108f986e0c1682267176c5
fofochi1 <alfonsobuzeta@gmail.com>
2022-02-08 16:48:27 -0500

    did listview

--- fanpage/lib/home_screen.dart
+++ fanpage/lib/home_screen.dart
@@ -1,7 +1,6 @@
 // ignore: import_of_legacy_library_into_null_safe
 // ignore_for_file: prefer_const_constructors
 
-import 'dart:html';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fanpage/post_screen.dart';
@@ -45,59 +44,59 @@ class _HomeScreenState extends State<HomeScreen> {
     if(user.uid == 'OHzUFdlHDsWJNRNlLSyWe2cgWxo2'){
       admin = true;
     }
-    if(admin){
       return buildButton(context, admin);
-    }
     
-    return Scaffold(
-      appBar: AppBar(
-        leading: null,
-        actions: <Widget>[
-          IconButton(
-              icon: Icon(Icons.close),
-              onPressed: () {
-                _auth.signOut();
-                Navigator.pop(context);
+    
+    // return Scaffold(
+    //   appBar: AppBar(
+    //     leading: null,
+    //     actions: <Widget>[
+    //       IconButton(
+    //           icon: Icon(Icons.close),
+    //           onPressed: () {
+    //             _auth.signOut();
+    //             Navigator.pop(context);
 
-                //Implement logout functionality
-              }),      
+    //             //Implement logout functionality
+    //           }),      
 
 
 
-                // ElevatedButton(
-                //   onPressed: () {
-                //     Navigator.push(
-                //       context,
-                //       MaterialPageRoute(builder: (context) =>  PostScreen()),
-                //     );
-                //   },
-                //   child: Icon(Icons.add, color: Colors.white),
-                //   style: ElevatedButton.styleFrom(
-                //     shape: CircleBorder(),
-                //     padding: EdgeInsets.all(20),
-                //     primary: Colors.blue, // <-- Button color
-                //     onPrimary: Colors.red, // <-- Splash color
-                //     ),
-                //   )
+    //             // ElevatedButton(
+    //             //   onPressed: () {
+    //             //     Navigator.push(
+    //             //       context,
+    //             //       MaterialPageRoute(builder: (context) =>  PostScreen()),
+    //             //     );
+    //             //   },
+    //             //   child: Icon(Icons.add, color: Colors.white),
+    //             //   style: ElevatedButton.styleFrom(
+    //             //     shape: CircleBorder(),
+    //             //     padding: EdgeInsets.all(20),
+    //             //     primary: Colors.blue, // <-- Button color
+    //             //     onPrimary: Colors.red, // <-- Splash color
+    //             //     ),
+    //             //   )
 
 
                   
-        ],
-        title: Text("Welcome"),
-        backgroundColor: Colors.lightBlueAccent,
-      ),
-      body: Center(
-        child: Text(
-          "Welcome User",
-          style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-        ),
-      ),
-    );
+    //     ],
+    //     title: Text("Welcome"),
+    //     backgroundColor: Colors.lightBlueAccent,
+    //   ),
+    //   body: Center(
+    //     child: Text(
+    //       "Welcome User",
+    //       style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+    //     ),
+    //   ),
+    // );
   }
 
   Widget buildButton(BuildContext context, bool admin){
     final dbRef = FirebaseDatabase.instance.reference().child('posts');
     List<Map<dynamic, dynamic>> lists = [];
+    final DocumentSnapshot document;
 
     if(admin){
     return Scaffold(
@@ -115,31 +114,31 @@ class _HomeScreenState extends State<HomeScreen> {
             ],
       ),
 
-      //  body: StreamBuilder(
-      //    stream: FirebaseFirestore.instance.collection('posts').snapshots(),
-      //    builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot){
-      //      if(!snapshot.hasData){
-      //        return Center(
-      //          child: CircularProgressIndicator(),
-      //          );
-      //      }
+       body: StreamBuilder(
+         stream: FirebaseFirestore.instance.collection('posts').snapshots(),
+         builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot){
+           if(!snapshot.hasData){
+             return Center(
+               child: CircularProgressIndicator(),
+               );
+           }
 
-      //      return ListView(
-      //        children: snapshot.data!.docs.map((document){
-      //           return Center(
-      //             child: Container(
-      //               width: MediaQuery.of(context).size.width/1.2,
-      //               height: MediaQuery.of(context).size.height/6,
-      //               child: Text(document['post']),
+           return ListView(
+             children: snapshot.data!.docs.map((document){
+                return Center(
+                  child: Container(
+                    width: MediaQuery.of(context).size.width/1.2,
+                    height: MediaQuery.of(context).size.height/6,
+                    child: Text(document['post']),
                      
                             
-      //               ),
-      //           );
-      //        },
-      //      ).toList(),
-      //      );
-      //      }
-      //  ),
+                    ),
+                );
+             },
+           ).toList(),
+           );
+           }
+       ),
 
       bottomNavigationBar: ElevatedButton(
                   onPressed: () {
@@ -173,33 +172,31 @@ class _HomeScreenState extends State<HomeScreen> {
               }),      
             ],
       ),
-      body: FutureBuilder(
-            future: dbRef.once(),
-            builder: (context, AsyncSnapshot<DataSnapshot> snapshot) {
-              if (snapshot.hasData) {
-                lists.clear();
-                Map<dynamic, dynamic> values = snapshot.data!.value;
-                values.forEach((key, values) {
-                  lists.add(values);
-                });
-                return new ListView.builder(
-                    shrinkWrap: true,
-                    itemCount: lists.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      return Card(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text("Name: " + lists[index]["name"]),
-                            Text("Age: " + lists[index]["age"]),
-                            Text("Type: " + lists[index]["type"]),
-                          ],
-                        ),
-                      );
-                    });
-              }
-              return CircularProgressIndicator();
-            })
+      body: StreamBuilder(
+         stream: FirebaseFirestore.instance.collection('posts').snapshots(),
+         builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot){
+           if(!snapshot.hasData){
+             return Center(
+               child: CircularProgressIndicator(),
+               );
+           }
+
+           return ListView(
+             children: snapshot.data!.docs.map((document){
+                return Center(
+                  child: Container(
+                    width: MediaQuery.of(context).size.width/1.2,
+                    height: MediaQuery.of(context).size.height/6,
+                    child: Text(document['post']),
+                     
+                            
+                    ),
+                );
+             },
+           ).toList(),
+           );
+           }
+       ),
 
       );
     }
