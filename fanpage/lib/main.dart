d055409e4e9ac7cc23037f24ca836d98b589907a
fofochi1 <alfonsobuzeta@gmail.com>
2022-02-17 14:21:54 -0500

    Added Splash Screen Widget

--- fanpage/lib/main.dart
+++ fanpage/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'welcome_screen.dart';
@@ -9,7 +10,8 @@ import 'post_screen.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(MyApp());
+  runApp(SplashScreen());
+  // runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -27,8 +29,24 @@ class MyApp extends StatelessWidget {
         'home_screen': (context) => HomeScreen(),
         'post_screen': (context) => PostScreen()
       },
+      
     );
   }
 }
 
+class SplashScreen extends StatelessWidget{
+  const SplashScreen({Key? key}) : super(key: key);
 
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+    
+    home: AnimatedSplashScreen(
+      splash: Image.asset('assets/alfonso.png'),
+      duration: 3000,
+      backgroundColor: Colors.blue,
+      nextScreen: const MyApp(),
+    ),
+    );
+  }
+}
